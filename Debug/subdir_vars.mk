ac6218d62cc13b7dfdf5762dd11cd4072d82ed2d
SteveHuang <steve.huang@globalmems.com>
2015-04-15 16:48:04 +0800

    kick off

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../freertos_demo_ccs.cmd 
+
+C_SRCS += \
+../freertos_t01.c \
+../led_task.c \
+../startup_ccs.c \
+../switch_task.c 
+
+OBJS += \
+./freertos_t01.obj \
+./led_task.obj \
+./startup_ccs.obj \
+./switch_task.obj 
+
+C_DEPS += \
+./freertos_t01.pp \
+./led_task.pp \
+./startup_ccs.pp \
+./switch_task.pp 
+
+C_DEPS__QUOTED += \
+"freertos_t01.pp" \
+"led_task.pp" \
+"startup_ccs.pp" \
+"switch_task.pp" 
+
+OBJS__QUOTED += \
+"freertos_t01.obj" \
+"led_task.obj" \
+"startup_ccs.obj" \
+"switch_task.obj" 
+
+C_SRCS__QUOTED += \
+"../freertos_t01.c" \
+"../led_task.c" \
+"../startup_ccs.c" \
+"../switch_task.c" 
+
+
